6c4781c1b2d59714d6c5e674aa10160500376789
ThomasGijsbers <thomasgijsbers@outlook.com>
2022-10-05 15:29:16 +0200

    add gfx init for SDL2

---
+++ include/gfx/init.h
@@ -0,0 +1,18 @@
+/*******************************************************************************************************************************
+ *
+ *   @file		init.h
+ *   @brief		
+ *
+ *   @date		2022/10/05
+ *   @author	Thomas Gijsbers (thomasgijsbers@outlook.com)
+ *
+ *   @copyright		  ©	2022 ― Thomas Gijsbers
+ *						All rights reserved.
+ *
+ ******************************************************************************************************************************/
+#ifndef INIT_H
+#define INIT_H
+
+#include "commondefs.h"
+
+#endif /* INIT_H */
